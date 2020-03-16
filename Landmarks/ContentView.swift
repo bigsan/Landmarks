bd6ef93a4357e4a273430bbd1f4bcdde2e5c845d
San Chen <bigsan.chen@gmail.com>
2020-03-16 18:41:37 +0800

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by San Chen on 2020/3/16.
+//  Copyright © 2020 cycu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
